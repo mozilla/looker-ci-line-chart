b1dd89e780ded4006e2cce05bd1005b8a8770073
Mike Williams <mwilliams@mozilla.com>
2022-04-14 17:05:03 -0400

    add configurable field names; fix redraw bug

--- src/ci-line-chart.js
+++ src/ci-line-chart.js
@@ -1,12 +1,80 @@
 import * as d3 from "d3";
 
 const vis = {
-  create: (element, config) => {
-    // TODO: styles in here?
-    // TODO: move some general setup to this fn?
+  options: {
+    field_x: {
+      type: "string",
+      label: "X Axis Field",
+      display: "select",
+      placeholder: "Submission Date",
+      order: 0,
+      values: [],
+    },
+    field_y: {
+      type: "string",
+      label: "Y Axis Field",
+      display: "select",
+      placeholder: "Percentile",
+      order: 1,
+      values: [],
+    },
+    ci_lower: {
+      type: "string",
+      label: "Confidence Interval Lower",
+      display: "select",
+      placeholder: "Percentile Lower",
+      order: 2,
+      values: [],
+    },
+    ci_upper: {
+      type: "string",
+      label: "Confidence Interval Upper",
+      display: "select",
+      placeholder: "Percentile Upper",
+      order: 3,
+      values: [],
+    },
+    group_field: {
+      type: "string",
+      label: "Field to Group By",
+      display: "select",
+      placeholder: "Branch",
+      order: 4,
+      default: undefined,
+      values: [],
+    },
   },
 
-  updateAsync: (data, element, config, queryResponse, details, done) => {
+  create (element, config) {
+    // TODO: styles in here?
+    // TODO: move some general setup to this fn?
+    this.svg = d3.select("#vis").append("svg")
+  },
+
+  updateAsync (data, element, config, queryResponse, details, done) {
+    this.clearErrors();
+
+    // Throw some errors and exit if the shape of the data isn't what this chart needs.
+    // TODO: more error checks
+    if (queryResponse.fields.dimensions.length === 0) {
+      this.addError({
+        title: "No Dimensions",
+        message: "This chart requires dimensions.",
+      });
+      return;
+    }
+    
+    // Fill in select options based on fields available
+    const dim_options = queryResponse.fields.dimensions.map(d => ({ [`${d.label_short}`]: `${d.name}` }));
+    const measure_options = queryResponse.fields.measures.map(d => ({ [`${d.label_short}`]: `${d.name}` }));
+    this.options.field_x.values = [...dim_options, ...measure_options];
+    this.options.field_y.values = measure_options;
+    this.options.ci_lower.values = measure_options;
+    this.options.ci_upper.values = measure_options;
+    this.options.group_field.values = [...dim_options, ...measure_options];
+    // register options with parent page to update visConfig
+    this.trigger('registerOptions', this.options);
+
     // console.log(data);
     // console.log(queryResponse);
     const width = element.clientWidth;
@@ -18,9 +86,8 @@ const vis = {
       left: 50,
     };
 
-    const svg = d3
-      .select("#vis")
-      .append("svg")
+    const svg = this.svg
+      .html('')
       .attr("width", "100%")
       .attr("height", "100%")
       .append("g")
@@ -35,19 +102,26 @@ const vis = {
     // TODO: dynamically get the correct fields from the data
     // 	(the user should be able to select these fields from the Gear menu
     //	 and we retrieve them here)
+    if (!(config.field_x && config.field_y && config.ci_lower && config.ci_upper)) {
+      this.addError({
+        title: "Check Config",
+        message: "This chart requires fields to be configured.",
+      });
+      return;
+    }
     const d3data = data.map((row) => ({
-      x: new Date(row[queryResponse.fields.dimensions[0].name].value),
-      y: row[queryResponse.fields.measures[0].name].value,
-      CI_left: row[queryResponse.fields.measures[1].name].value,
-      CI_right: row[queryResponse.fields.measures[2].name].value,
-      group_val: row[queryResponse.fields.dimensions[1].name].value,
+      x: new Date(row[config.field_x].value),
+      y: row[config.field_y].value,
+      CI_left: row[config.ci_lower].value,
+      CI_right: row[config.ci_upper].value,
+      group_val: config.group_field ? row[config.group_field].value : 'NONE',
     }));
 
     // console.log(d3data);
 
-    // using d3.extent to do this automatically, but leaving in for now
-    // in case this has some advantage...
-    // calculate X domain
+    // using d3.extent to do this automatically, but leaving calcs here
+    // for now in case this has some advantage...
+    // Calculate X domain
     // const dataX = d3data.map(d => d.x);
     // const minX = Math.floor(Math.min(...dataX));
     // const maxX = Math.ceil(Math.max(...dataX));
@@ -56,8 +130,8 @@ const vis = {
     const minY = Math.floor(Math.min(...d3data.map((d) => d.CI_left)));
     const maxY = Math.ceil(Math.max(...d3data.map((d) => d.CI_right)));
 
-    console.log(`X: [${minX}, ${maxX}]`);
-    console.log(`Y: [${minY}, ${maxY}]`);
+    // console.log(`X: [${minX}, ${maxX}]`);
+    // console.log(`Y: [${minY}, ${maxY}]`);
 
     // Add X axis --> it is a date format
     const x = d3
@@ -82,13 +156,15 @@ const vis = {
 
     // Setup lines for each group
     // TODO: dynamic lookup of the grouping field
-    const groupName = queryResponse.fields.dimensions[1].name;
-    const allGroups = data.map((d) => d[groupName].value);
-    const groups = [...new Set(allGroups)]; // unique only
+    let groups = ['NONE'];
+    if (config.group_field) {
+      const allGroups = data.map((d) => d[config.group_field].value);
+      groups = [...new Set(allGroups)]; // unique only
+    }
 
     // TODO: dynamic color range (user configurable?)
     // const color = d3.scaleOrdinal()
-    // 	.domain(groups).range(['#cce5df88', '#ff000088']);
+    // 	.domain(groups).range(['#cce5df68', '#ff000068']);
 
     groups.forEach((group, idx) => {
       // Filter data to the current group
@@ -102,7 +178,7 @@ const vis = {
         .datum(subData)
         // .attr("fill", "#cce5df")
         // .attr("fill", d => color(d.key))
-        .attr("fill", idx === 0 ? "#cce5df88" : "#ff000088")
+        .attr("fill", idx === 0 ? "#cce5df68" : "#ff000068")
         .attr("stroke", "none")
         .attr(
           "d",
